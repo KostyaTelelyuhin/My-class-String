269e137ce0abbbd9989f1a09212aa333924a0418
KostyaTelelyuhin <142903343+KostyaTelelyuhin@users.noreply.github.com>
2024-08-26 17:30:08 +0300

    Initial Commit

---
+++ Class string/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Class string
+//
+//  Created by forcs2 on 26.08.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
